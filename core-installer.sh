085cf89a4974d3429ecfdc4da8a2d42aeb08bb32
Sahel Jalal <sahel@pokanop.com>
2023-02-27 16:00:38 -0800

    feat: migrate install scripts and organize

---
+++ core-installer.sh
@@ -0,0 +1,113 @@
+#!/bin/zsh
+#
+# Core installer to set up dev environment
+set -e
+
+setup_paths() {
+  # rainsphere dev paths
+  RS_DEV_PATH="${RS_DEV_PATH:-$(prompt_user "Enter the absolute path to your rainsphere dev directory" "$PWD")}"
+  RS_DEV_ONBOARDING_PATH="${RS_DEV_ONBOARDING_PATH:-$RS_DEV_PATH/rs-onboarding-scripts}"
+  RS_DEV_SCRIPTS_PATH="${RS_DEV_SCRIPTS_PATH:-$RS_DEV_ONBOARDING_PATH/scripts}"
+}
+
+setup_nostromo() {
+  # Prevent the cloned repository from having insecure permissions. Failing to do
+  # so causes compinit() calls to fail with "command not found: compdef" errors
+  # for users with insecure umasks (e.g., "002", allowing group writability). Note
+  # that this will be ignored under Cygwin by default, as Windows ACLs take
+  # precedence over umasks except for filesystems mounted with option "noacl".
+  umask g-w,o-w
+
+  touch ~/.zshrc
+
+  fmt_info "Installing Homebrew..."
+  command_exists brew || {
+    /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)" || {
+      fmt_error "homebrew installation failed"
+      exit 1
+    }
+
+    eval "$(/opt/homebrew/bin/brew shellenv)"
+  }
+
+  fmt_info "Installing nostromo..."
+  command_exists nostromo || brew install pokanop/pokanop/nostromo || {
+    fmt_error "nostromo installation failed"
+    exit 1
+  }
+
+  fmt_info "Initializing nostromo..."
+  nostromo init || {
+    fmt_error "nostromo initialization failed"
+    exit 1
+  }
+
+  ostype=$(uname)
+  if [ -z "${ostype%CYGWIN*}" ] && git --version | grep -Eq 'msysgit|windows'; then
+    fmt_error "Windows/MSYS Git is not supported on Cygwin"
+    fmt_error "Make sure the Cygwin git package is installed and is first on the \$PATH"
+    exit 1
+  fi
+}
+
+setup_dev() {
+  fmt_info "Cloning development scripts..."
+  dir_exists $RS_DEV_PATH/rs-onboarding-scripts || {
+    pushd $RS_DEV_PATH
+    git clone git@github.com:rainsphere-ai/rs-onboarding-scripts.git || {
+      fmt_error "rs-onboarding-scripts clone failed"
+      exit 1
+    }
+    popd
+  }
+
+  [ -z $RS_RAINSPHERE_SCRIPT_LOADED ] && {
+    source $RS_DEV_ONBOARDING_PATH/rainsphere.sh || {
+      fmt_error "rainsphere.sh sourcing failed"
+      exit 1
+    }
+  }
+ 
+  fmt_info "Docking nostromo manifests..."
+  nostromo dock git@github.com:rainsphere-ai/rs-onboarding-scripts.git || {
+    fmt_error "nostromo docking failed"
+    exit 1
+  }
+
+  source ~/.zshrc
+
+  fmt_info "Setting up dev tools..."
+  dev setup tools || {
+    fmt_error "dev tools installation failed"
+    exit 1
+  }
+
+  fmt_info "Setting up dev env..."
+  dev setup env || {
+    fmt_error "dev env installation failed"
+    exit 1
+  }
+}
+
+# shellcheck disable=SC2183  # printf string has more %s than arguments ($FMT_RAINBOW expands to multiple arguments)
+print_success() {
+  printf '\n'
+  fmt_info "Hooray! Your rainsphere development environment is ready to go 🎉"
+  printf '\n'
+  printf '\n'
+  printf '%s\n' "• Join our $(fmt_link "Slack channel" https://rainsphere.slack.com/archives/C02G25XC00M)"
+  printf '%s\n' $FMT_RESET
+}
+
+main() {
+  setup_scripts
+  setup_colors
+  
+  setup_paths
+  setup_nostromo
+  setup_dev
+
+  print_success
+}
+
+main "$@"
---
+++ format.sh
@@ -0,0 +1,153 @@
+# This function uses the logic from supports-hyperlinks[1][2], which is
+# made by Kat Marchán (@zkat) and licensed under the Apache License 2.0.
+# [1] https://github.com/zkat/supports-hyperlinks
+# [2] https://crates.io/crates/supports-hyperlinks
+#
+# Copyright (c) 2021 Kat Marchán
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+supports_hyperlinks() {
+  # $FORCE_HYPERLINK must be set and be non-zero (this acts as a logic bypass)
+  if [ -n "$FORCE_HYPERLINK" ]; then
+    [ "$FORCE_HYPERLINK" != 0 ]
+    return $?
+  fi
+
+  # If stdout is not a tty, it doesn't support hyperlinks
+  is_tty || return 1
+
+  # DomTerm terminal emulator (domterm.org)
+  if [ -n "$DOMTERM" ]; then
+    return 0
+  fi
+
+  # VTE-based terminals above v0.50 (Gnome Terminal, Guake, ROXTerm, etc)
+  if [ -n "$VTE_VERSION" ]; then
+    [ $VTE_VERSION -ge 5000 ]
+    return $?
+  fi
+
+  # If $TERM_PROGRAM is set, these terminals support hyperlinks
+  case "$TERM_PROGRAM" in
+  Hyper|iTerm.app|terminology|WezTerm) return 0 ;;
+  esac
+
+  # kitty supports hyperlinks
+  if [ "$TERM" = xterm-kitty ]; then
+    return 0
+  fi
+
+  # Windows Terminal also supports hyperlinks
+  if [ -n "$WT_SESSION" ]; then
+    return 0
+  fi
+
+  # Konsole supports hyperlinks, but it's an opt-in setting that can't be detected
+  # https://github.com/ohmyzsh/ohmyzsh/issues/10964
+  # if [ -n "$KONSOLE_VERSION" ]; then
+  #   return 0
+  # fi
+
+  return 1
+}
+
+# Adapted from code and information by Anton Kochkov (@XVilka)
+# Source: https://gist.github.com/XVilka/8346728
+supports_truecolor() {
+  case "$COLORTERM" in
+  truecolor|24bit) return 0 ;;
+  esac
+
+  case "$TERM" in
+  iterm           |\
+  tmux-truecolor  |\
+  linux-truecolor |\
+  xterm-truecolor |\
+  screen-truecolor) return 0 ;;
+  esac
+
+  return 1
+}
+
+setup_colors() {
+  # Only use colors if connected to a terminal
+  if ! is_tty; then
+    FMT_RAINBOW=""
+    FMT_RED=""
+    FMT_GREEN=""
+    FMT_YELLOW=""
+    FMT_BLUE=""
+    FMT_BOLD=""
+    FMT_RESET=""
+    return
+  fi
+
+  if supports_truecolor; then
+    FMT_RAINBOW="
+      $(printf '\033[38;2;255;0;0m')
+      $(printf '\033[38;2;255;97;0m')
+      $(printf '\033[38;2;247;255;0m')
+      $(printf '\033[38;2;0;255;30m')
+      $(printf '\033[38;2;77;0;255m')
+      $(printf '\033[38;2;168;0;255m')
+      $(printf '\033[38;2;245;0;172m')
+    "
+  else
+    FMT_RAINBOW="
+      $(printf '\033[38;5;196m')
+      $(printf '\033[38;5;202m')
+      $(printf '\033[38;5;226m')
+      $(printf '\033[38;5;082m')
+      $(printf '\033[38;5;021m')
+      $(printf '\033[38;5;093m')
+      $(printf '\033[38;5;163m')
+    "
+  fi
+
+  FMT_RED=$(printf '\033[31m')
+  FMT_GREEN=$(printf '\033[32m')
+  FMT_YELLOW=$(printf '\033[33m')
+  FMT_BLUE=$(printf '\033[34m')
+  FMT_BOLD=$(printf '\033[1m')
+  FMT_RESET=$(printf '\033[0m')
+}
+
+fmt_link() {
+  # $1: text, $2: url, $3: fallback mode
+  if supports_hyperlinks; then
+    printf '\033]8;;%s\033\\%s\033]8;;\033\\\n' "$2" "$1"
+    return
+  fi
+
+  case "$3" in
+  --text) printf '%s\n' "$1" ;;
+  --url|*) fmt_underline "$2" ;;
+  esac
+}
+
+fmt_underline() {
+  is_tty && printf '\033[4m%s\033[24m\n' "$*" || printf '%s\n' "$*"
+}
+
+# shellcheck disable=SC2016 # backtick in single-quote
+fmt_code() {
+  is_tty && printf '`\033[2m%s\033[22m`\n' "$*" || printf '`%s`\n' "$*"
+}
+
+fmt_error() {
+  printf '%sError: %s%s\n' "${FMT_BOLD}${FMT_RED}" "$*" "$FMT_RESET" >&2
+}
+
+fmt_info() {
+  printf '%s%s%s\n' "${FMT_BOLD}" "$*" "$FMT_RESET"
+}
