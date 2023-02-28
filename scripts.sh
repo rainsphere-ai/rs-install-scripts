a4d27f78bbf4ab1a658228e84aa95a5a95ac7536
Adib Jalal <adib@rainsphere.ai>
2023-02-27 17:58:40 -0800

    fix: remove array in sh, add source helper

--- scripts.sh
+++ scripts.sh
@@ -1,5 +1,17 @@
 BASEDIR=$(dirname "$0")
 RS_TMP_PATH=/tmp/rainsphere
 
-source $BASEDIR/format.sh || source $RS_TMP_PATH/format.sh
-source $BASEDIR/utils.sh || source $RS_TMP_PATH/utils.sh
+source_script() {
+  local script=$1
+  local fallback=$2
+
+  [ -f $script ] && source $script || {
+    [ -f $fallback ] && source $fallback || {
+      printf "failed to source script: $script\n"
+      exit 1
+    }
+  }
+}
+
+source_script $BASEDIR/format.sh $RS_TMP_PATH/format.sh
+source_script $BASEDIR/utils.sh $RS_TMP_PATH/utils.sh
